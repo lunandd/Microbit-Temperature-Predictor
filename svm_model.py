6d5ad2c64520d72402585240cd990397b67b3d6f
Pavlos-Efstathiou <jimosmekma@gmail.com>
2021-03-16 18:52:22 +0200

    The graph is better and the console displays the average temperature

--- svm_model.py
+++ svm_model.py
@@ -7,8 +7,11 @@ data = pd.read_csv("temp-data.csv")
 
 predict = "temp"
 
-X = np.array(data.drop([predict], axis=1))
+X = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
+
+# y.reshape(1, -1)
+
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 
 clf = svm.SVC(kernel="rbf", C=100)
@@ -18,8 +21,8 @@ y_pred = clf.predict(x_test)
 
 
 acc = metrics.accuracy_score(y_test, y_pred)
-
-print(f"Accuracy: {acc}/1.0\n Predictions: {y_pred}\n")
+print(f"Accuracy: {acc}/1.0\nPredictions: {y_pred}\n")
+print(f"Average Temperature: {np.mean(y_pred)}")
 
 predictions_df = pd.DataFrame(data=y_pred.flatten())
-predictions_df.plot(kind="kde")
+predictions_df.plot(title="Temperature", kind="kde", fontsize=10)
