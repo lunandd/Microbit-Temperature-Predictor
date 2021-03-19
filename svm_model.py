c35c1dfb1264a7c597962521aaf4748609b37af1
Pavlos-Efstathiou <jimosmekma@gmail.com>
2021-03-19 14:52:47 +0200

    Added a check for unsuported delimiters when using arguments and did some minor changes

--- svm_model.py
+++ svm_model.py
@@ -1,25 +1,36 @@
+from os import sep
+from sklearn import svm, metrics
 import pandas as pd
 import numpy as np
 import sklearn
 import sys
-from sklearn import datasets, svm, metrics
 
+special_delimiters = {
+    "space": " ",
+    "tab": "\t",
+    "tab2": "\t\t"
+}
 # Command line arguments are experimental and not complete yet. They mostly don't work
 # and shouldn't be used until they're decent
 for i in range(2, 5):
     if len(sys.argv) < 3:
+        print("No arguments supplied, will use the input function")
         file_name = str(input("What's the name of the file that contains the temperature data of your micro:bit?\n"))
-        separator = str(input("What's the separator of your file?\n"))
+        delimiter = str(input("What's the delimiter of your file?\n"))
         predict = str(input("Name of the y column?\n"))
         break
     elif sys.argv[1] == "-args" and sys.argv[i] != None:
+        print("Arguments will be used")
         file_name = sys.argv[2]
-        separator = sys.argv[3]
+        delimiter = sys.argv[3]
         predict = sys.argv[4]
+        for key, value in special_delimiters.items():
+            if key == delimiter:
+                delimiter = value
     else:
         continue
 
-data = pd.read_csv(file_name, sep=separator)
+data = pd.read_csv(file_name, sep=delimiter, encoding="ascii")
 
 X = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
@@ -31,9 +42,7 @@ clf.fit(x_train, y_train)
 
 y_pred = clf.predict(x_test)
 
-acc = metrics.accuracy_score(y_test, y_pred)
-print(f"Accuracy: {acc}/1.0\nPredictions: {y_pred}\n")
-print(f"Average Temperature: {np.mean(y_pred)}")
+accuracy = metrics.accuracy_score(y_test, y_pred)
 
-predictions_df = pd.DataFrame(data=y_pred.flatten())
-predictions_df.plot(title="Temperature", kind="kde", fontsize=10)
+print(f"Accuracy: {accuracy}/1.0\nPredictions: {y_pred}\n")
+print(f"Average Temperature: {np.mean(y_pred)}")
