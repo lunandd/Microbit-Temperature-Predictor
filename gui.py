5b5d5ee8097bde4617ad25412280c8bfc2d219c1
Pavlos-Efstathiou <jimosmekma@gmail.com>
2021-03-23 10:48:56 +0200

    Added error handling and improved the GUI a little

--- gui.py
+++ gui.py
@@ -1,32 +1,29 @@
 import tkinter as tk
 import svm_gui
 
-# text = list("")
-# def click(user_input, input_arr):
-#     input_arr.clear()
-#     for i in range(0, len(user_input)):
-#         input_arr.append(user_input[i].get())
-#         print(input_arr)
-
 window = tk.Tk()
 window.title("Micro:bit Temperature Predictor")
+error_or_output_label = tk.Label()
 window.configure(bg="#161616")
+window.geometry("550x450")
+window.iconbitmap("")
 
-tk.Label(window, text="file that contains the temperature data of your micro:bit:", bg="#161616", fg="#f7f7f7", font="none 12 bold").grid(row=0, column=0, sticky=tk.W)
-file_name = tk.Entry(window, width=20, bg="#f7f7f7")
+tk.Label(window, text="File that contains the temperature data of your micro:bit", bg="#161616", fg="#f7f7f7", font="none 12 bold").grid(row=0, column=0, sticky=tk.W)
+file_name = tk.Entry(window, width=20, bg="#191919", fg="#f7f7f7")
 file_name.grid(row=1, column=0, sticky=tk.W)
 
-tk.Label(window, text="Delimiter used:", bg="#161616", fg="#f7f7f7", font="none 12 bold").grid(row=2, column=0, sticky=tk.W)
-delimiter = tk.Entry(window, width=20, bg="#f7f7f7")
+tk.Label(window, text="Delimiter used", bg="#161616", fg="#f7f7f7", font="none 12 bold").grid(row=2, column=0, sticky=tk.W)
+delimiter = tk.Entry(window, width=20, bg="#191919", fg="#f7f7f7")
 delimiter.grid(row=3, column=0, sticky=tk.W)
 
-tk.Label(window, text="Name of the y column:", bg="#161616", fg="#f7f7f7", font="none 12 bold").grid(row=4, column=0, sticky=tk.W)
-predict = tk.Entry(window, width=20, bg="#f7f7f7")
+tk.Label(window, text="Name of the y column", bg="#161616", fg="#f7f7f7", font="none 12 bold").grid(row=4, column=0, sticky=tk.W)
+predict = tk.Entry(window, width=20, bg="#191919", fg="#f7f7f7")
 predict.grid(row=5, column=0, sticky=tk.W)
 
 
 # https://stackoverflow.com/questions/6920302/how-to-pass-arguments-to-a-button-command-in-tkinter
-tk.Button(window,text="Submit", width=6,
-command=lambda: svm_gui.svm_gui(window, file_name.get(), delimiter.get(), predict.get())).grid(row=7, column=0, sticky=tk.W) # click([file_name, delimiter, predict], text)"""
+tk.Button(window, text="Submit", width=6, bg="#161616", fg="#f7f7f7", 
+command=lambda: svm_gui.svm_gui(window, file_name.get(), delimiter.get(), predict.get(), error_or_output_label)
+).grid(row=9, column=0, stick=tk.SW, pady=9)
 
 window.mainloop()
--- svm_gui.py
+++ svm_gui.py
@@ -3,8 +3,8 @@ import tkinter as tk
 import pandas as pd
 import numpy as np
 import sklearn
-
-def svm_gui(window, filename, delimiter, predict):
+ 
+def svm_gui(window: tk.Tk, filename: str, delimiter: str, predict: str, output_label: tk.Label):
     special_delimiters = {
         "space": " ",
         "tab": "\t",
@@ -14,23 +14,39 @@ def svm_gui(window, filename, delimiter, predict):
         "pipe": "|",
         "colon": ":"
     }
+    window.geometry("650x650")
     for key, value in special_delimiters.items():
         if key == delimiter:
             delimiter = value
-    data = pd.read_csv(filename, sep=delimiter)
+    if ".csv" in filename:
+        pass
+    else:
+        filename += "".join(".csv")
 
-    X = np.array(data.drop([predict], 1))
-    y = np.array(data[predict])
+    try:
+        data = pd.read_csv(filename, sep=delimiter)
+    except (FileNotFoundError) as e:
+        output_label = tk.Label(window, text=f"{e}", bg="#161616", fg="#cc0000",
+        font="none 12 bold").grid(row=10, column=0, sticky=tk.W)
+    try:
+        X = np.array(data.drop([predict], 1))
+        y = np.array(data[predict])
 
-    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+        x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 
-    clf = svm.SVC(kernel="rbf", C=100)
-    clf.fit(x_train, y_train)
+        clf = svm.SVC(kernel="rbf", C=100)
+        clf.fit(x_train, y_train)
 
-    y_pred = clf.predict(x_test)
+        y_pred = clf.predict(x_test)
 
-    accuracy = metrics.accuracy_score(y_test, y_pred)
+        accuracy = metrics.accuracy_score(y_test, y_pred)
 
-    print(f"Accuracy: {accuracy}/1.0\nPredictions: {y_pred}\n")
-    print(f"Average Temperature: {np.mean(y_pred)}")
-    tk.Label(window, text=f"Accuracy: {accuracy}/1.0\nPredictions: {y_pred}\nAverage Temperature: {np.mean(y_pred)}", bg="#161616", fg="#f7f7f7", font="none 12 bold").grid(row=9, column=0, sticky=tk.W)
+        print(f"Accuracy: {accuracy}/1.0\nPredictions: {y_pred}\n")
+        print(f"Average Temperature: {np.mean(y_pred)}")
+
+        output_label = tk.Label(window, text=f"Accuracy:\n{accuracy}/1.0\nPredictions: {y_pred}\nAverage Temperature: {np.mean(y_pred)}",
+        bg="#161616", fg="#f7f7f7", font="none 12 bold").grid(row=10, column=0, sticky=tk.W)
+    
+    except (Exception, TypeError) as e:
+        output_label = tk.Label(window, text=f"{e}", bg="#161616", fg="#cc0000",
+        font="none 12 bold").grid(row=10, column=0, sticky=tk.W)
